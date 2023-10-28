33caf630fac89fcaca2be8a73a247b2b7f73add4
Tenzin Nyima <tnyima585@gmail.com>
2023-10-29 03:04:02 +0530

    Project Commit

---
+++ src/Page/Dashboard/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header({ setIsAdding}) {
+  return (
+    <header>
+      <h1>Employee Management System</h1>
+      <div>
+        <button onClick={() => setIsAdding(true)} className='round-button'>Add Button</button>
+      
+      </div>
+
+    </header>
+    
+  );
+}
+
+export default Header;

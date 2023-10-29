0aacbe0c6b26e0a65b836f6e19c9046189ec9f2e
Tenzin Nyima <tnyima585@gmail.com>
2023-10-30 00:34:05 +0530

    updated commit

--- src/data/index.js
+++ src/data/index.js
@@ -1,56 +1,56 @@
  const employeesData = [
     {
         id: 1,
-        firstName: 'Susan',
-        lastName: 'jordon',
-        email: 'susan@example.com',
+        firstName: 'Tenzin',
+        lastName: 'Nyima',
+        email: 'tenzin@example.com',
         salary: '95000',
         date: '2019-04-11'
     },
     {
         id: 2,
-        firstName: 'Adrienne',
-        lastName: 'Doak',
-        email: 'adrienne@example.com',
+        firstName: 'Hritik',
+        lastName: 'Roshan',
+        email: 'hritik@example.com',
         salary: '80000',
         date: '2019-04-17'
     },
     {
         id: 3,
-        firstName: 'Rolf',
-        lastName: 'Hegdal',
-        email: 'rolf@example.com',
+        firstName: 'Akshay',
+        lastName: 'Khanna',
+        email: 'akshay@example.com',
         salary: '79000',
         date: '2019-05-01'
     },
     {
         id: 4,
-        firstName: 'Kent',
-        lastName: 'Rosner',
-        email: 'kent@example.com',
+        firstName: 'Priety',
+        lastName: 'Zinta',
+        email: 'priety@example.com',
         salary: '56000',
         date: '2019-05-03'
       },
       {
         id: 5,
-        firstName: 'Arsenio',
-        lastName: 'Grant',
-        email: 'arsenio@example.com',
+        firstName: 'Justin',
+        lastName: 'Bieber',
+        email: 'justin@example.com',
         salary: '65000',
         date: '2019-06-13'
       },
       {
         id: 6,
-        firstName: 'Laurena',
-        lastName: 'Lurie',
-        email: 'laurena@example.com',
+        firstName: 'Dua',
+        lastName: 'Lipa',
+        email: 'dua@example.com',
         salary: '120000',
         date: '2019-07-30'
       },
       {
         id: 7,
         firstName: 'George',
-        lastName: 'Tallman',
+        lastName: 'Michael',
         email: 'george@example.com',
         salary: '90000',
         date: '2019-08-15'
@@ -58,23 +58,23 @@
       {
         id: 8,
         firstName: 'Jesica',
-        lastName: 'Watlington',
+        lastName: 'Simpson',
         email: 'jesica@example.com',
         salary: '60000',
         date: '2019-10-10'
       },
       {
         id: 9,
-        firstName: 'Matthew',
-        lastName: 'Warren',
-        email: 'matthew@example.com',
+        firstName: 'Suniddhi',
+        lastName: 'chauhan',
+        email: 'suniddhi@example.com',
         salary: '71000',
         date: '2019-10-15'
       },
       {
         id: 10,
         firstName: 'Lyndsey',
-        lastName: 'Follette',
+        lastName: 'Lohan',
         email: 'lyndsey@example.com',
         salary: '110000',
         date: '2020-01-15'
